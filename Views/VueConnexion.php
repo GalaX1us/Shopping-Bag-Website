eb488efcfc620805ef3858111ffe6a0617ba5bb5
p2007049 <luc.brun@etu.univ-lyon1.fr>
2022-11-30 15:21:01 +0100

    début page connexion

---
+++ Views/VueConnexion.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Page de connexion</title>
+  </head>
+
+  <body>
+    <h1> Identification client </h1>
+    <p> Merci d'entrer votre identifiant et votre mot de passe pour accéder à votre espace client.
+        Si vous n'avez pas de compte client, vous pouvez en créer un ici gratuitement ici ! Enregistrement </p>
+
+        
+  </body>
+</html>
