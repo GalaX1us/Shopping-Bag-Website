ebaf8f6dbe5fc0f415a67e7fd3476e653376efef
Nathan BONADA <nathan.bonada@gmail.com>
2022-11-30 13:48:06 +0100

    basic changes

---
+++ Views/vueAccueil.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8" />
+        <link rel="stylesheet" href="Contenu/style.css" />
+        <title>ISIWEB4SHOP</title>
+    </head>
+    
+    <body>
+        <div id="global">
+            <header>
+                <a href="index.php"><h1 id="titreBlog">Mon Blog</h1></a>
+                <p>Je vous souhaite la bienvenue sur ce modeste blog.</p>
+            </header>
+            <div id="contenu">
+            </div>
+            <footer>
+                Blog réalisé avec PHP, HTML5 et CSS.
+            </footer>
+        </div>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,1 @@
+<?php require 'Views/vueAccueil.php';
