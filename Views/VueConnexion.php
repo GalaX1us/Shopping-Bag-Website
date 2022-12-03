fc414876b9e06ec31cdcb8d6b198f0e330e81fe3
p2007049 <luc.brun@etu.univ-lyon1.fr>
2022-12-03 10:29:41 +0100

    ajout d'un formulaire sur la page de connexion

--- Views/VueConnexion.php
+++ Views/VueConnexion.php
@@ -10,6 +10,12 @@
     <p> Merci d'entrer votre identifiant et votre mot de passe pour accéder à votre espace client.
         Si vous n'avez pas de compte client, vous pouvez en créer un ici gratuitement ici ! Enregistrement </p>
 
-        
+        <p><input type="text" name="MotDePass" placeholder ="Mot de passe"/></p> </p>
+
+        <p><input type="button" value="Continuer"></p>
+
+
+      
+      </form>
   </body>
 </html>
