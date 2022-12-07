6efdf821daf606e3801c10ad1078fb2b22b738b1
Clifly <aurelien.boissot@gmail.com>
2022-12-07 15:55:03 +0100

    ajout vueBoissons

---
+++ Views/VueBoissons.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8" />
+    <link rel="stylesheet" href="../Style/Style.css" />
+    <title>ISIWEB4SHOP</title>
+</head>
+
+<body>
+    <main>
+        <div class="image_produit">
+            <img src="../assets/theImperial.jpg" alt="Image du thé impérial.">
+        </div>
+        <div class="description_produit">
+            <h3>Thé Impérial</h3>
+            <p>Sachet de thé de qualité supérieure. 200 sachets par boîte</p>
+            <strong>Notre prix : 4.99€</strong>
+            <a href="#">[Acheter]</a>
+        </div>
+    </main>
+</body>
+
+</html>
