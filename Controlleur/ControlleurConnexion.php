71df4c750bc50ca4e2f438f70fbbbdc2ceceda97
p2007049 <luc.brun@etu.univ-lyon1.fr>
2022-12-19 11:49:14 +0100

    le loggin toujours en chantier (à debbuger et continuer) mais on progresse

--- Controlleur/ControlleurConnexion.php
+++ Controlleur/ControlleurConnexion.php
@@ -7,30 +7,9 @@ $password = $_POST['password'];
 require_once '../Modeles/Logins.php';
 
 
-
-echo $username; //debug
-echo $password; //debug
-
-echo "toto"; //debug
-
-require_once '../Modeles/Logins.php';
+$login = new Login();
+$user = $login->getLogin($username);
 
 
+echo $user[2]; //debug
 
-// // Connexion à la base de données
-// $db = new PDO('mysql:host=localhost;dbname=mydb', 'user', 'password');
-
-// // Vérification des identifiants
-// $req = $db->prepare('SELECT id FROM users WHERE username = :username AND password = :password');
-// $req->execute(array(
-//     'username' => $username,
-//     'password' => $password));
-
-// $result = $req->fetch();
-
-// if (!$result) {
-//     // Mauvais identifiants
-//     echo 'Mauvais identifiants';
-// } else {
-//     // Identifiants corrects
-//     echo 'Identifiants corrects';
--- Modeles/Logins.php
+++ Modeles/Logins.php
@@ -3,10 +3,11 @@ require_once '../Modeles/Modele.php';
 class Login extends Modele
 {
     //renvoie les infos sur un login
-    public function getLogin($username, $password)
+    public function getLogin($username)
     {
-        $sql = 'SELECT * FROM login WHERE username = ? AND password = ?';
-        $login = $this->executerRequete($sql, array($username, $password));
+        $sql = 'SELECT * FROM admin WHERE username = ?';
+        $login = $this->executerRequete($sql, array($username));
+        echo 'ici'; //debug
         return $login->fetch();  // Accès à la première ligne de résultat
     }
 
