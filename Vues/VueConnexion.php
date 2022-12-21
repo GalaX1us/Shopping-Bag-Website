b826e350e616c506c47fd00cb43dcce863d3bec4
Clifly <aurelien.boissot@gmail.com>
2022-12-21 18:30:06 +0100

    #

--- Vues/VueConnexion.php
+++ Vues/VueConnexion.php
@@ -6,7 +6,7 @@
     <p> Merci d'entrer votre identifiant et votre mot de passe pour accéder à votre espace client.
         Si vous n'avez pas de compte client, vous pouvez en créer un ici gratuitement ici ! Enregistrement </p>
 
-    <form method="post" action="../Controlleur/ControlleurConnexion.php"> <!-- bien creer le fichier au bon endroit -->
+    <form method="post" action="Controlleur/ControlleurConnexion.php"> <!-- bien creer le fichier au bon endroit -->
       <div class="mb-3">
         <input type="identifiant" name="username" placeholder="Entrez votre identifiant" class="form-control"  aria-describedby="emailHelp">
       </div>
