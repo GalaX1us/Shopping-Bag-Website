0e4ea52823e1b6a0c4882776f6fc03087e93186c
p2007049 <luc.brun@etu.univ-lyon1.fr>
2023-01-08 11:34:16 +0100

    ajout du caractère requis des champs pour créer le compte

--- Vues/VueCreerCompte.php
+++ Vues/VueCreerCompte.php
@@ -16,25 +16,25 @@ else
 
   <form method="post" action="index.php?action=CreerCompte"> 
         <div class="mb-3">
-          <input type="text" name="name" placeholder="prénom" class="form-control"  aria-describedby="emailHelp">
-          <input type="text" name="surname" placeholder="Nom" class="form-control" >
+          <input type="text" name="name" placeholder="prénom" class="form-control"  aria-required="true" aria-describedby="emailHelp">
+          <input type="text" name="surname" placeholder="Nom" class="form-control" aria-required="true">
         </div>
         <br>
         <div class="mb-3">
-          <input type="text" name="add1" placeholder="Adresse" class="form-control" > 
+          <input type="text" name="add1" placeholder="Adresse" class="form-control" aria-required="true"> 
           <input type="text" name="add2" placeholder="Complément d'adresse" class="form-control" >
-          <input type="text" name="city" placeholder="Ville" class="form-control" >
-          <input type="text" name="code" placeholder="code postal" class="form-control" >
+          <input type="text" name="city" placeholder="Ville" class="form-control" aria-required="true">
+          <input type="text" name="code" placeholder="code postal" class="form-control" aria-required="true">
         </div>
         <br>
         <div class="mb-3">
-          <input type="tel" name="phone" placeholder="Numéro de téléphone" class="form-control"  aria-describedby="emailHelp">
-          <input type="email" name="email" placeholder="Email" class="form-control" >
+          <input type="tel" name="phone" placeholder="Numéro de téléphone" class="form-control"  aria-describedby="emailHelp" aria-required="true">
+          <input type="email" name="email" placeholder="Email" class="form-control" aria-required="true">
         </div>
         <br>
         <div class="mb-3">
-        <input type="identifiant" name="username" placeholder="Identifiant" class="form-control"  aria-describedby="emailHelp">
-          <input type="password" name="password" placeholder="Mot de passe" class="form-control"  aria-describedby="emailHelp">
+        <input type="identifiant" name="username" placeholder="Identifiant" class="form-control"  aria-describedby="emailHelp" aria-required="true">
+          <input type="password" name="password" placeholder="Mot de passe" class="form-control"  aria-describedby="emailHelp" aria-required="true">
         </div>
         <br>
         <button type="submit" class="btn btn-primary btn-lg btn-block">Valider</button>
