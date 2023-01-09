2893e423897822dc86f5f246414f8f1d8ce6c62b
p2007049 <luc.brun@etu.univ-lyon1.fr>
2023-01-09 19:08:18 +0100

    rajout de la vérification du format du code postale et du numéro de téléphone

--- Vues/VueCreerCompte.php
+++ Vues/VueCreerCompte.php
@@ -24,11 +24,11 @@ else
           <input type="text" name="add1" placeholder="Adresse" class="form-control" aria-required="true" required> 
           <input type="text" name="add2" placeholder="Complément d'adresse" class="form-control" >
           <input type="text" name="city" placeholder="Ville" class="form-control" aria-required="true" required>
-          <input type="text" name="code" placeholder="code postal" class="form-control" aria-required="true" required>
+          <input type="text" name="code" placeholder="code postal" class="form-control" pattern="\d{5,5}"aria-required="true" required>
         </div>
         <br>
         <div class="mb-3">
-          <input type="tel" name="phone" placeholder="Numéro de téléphone" class="form-control"  aria-describedby="emailHelp" aria-required="true" required>
+          <input type="tel" name="phone" placeholder="Numéro de téléphone" class="form-control"  aria-describedby="emailHelp" pattern="\d{10,10}" aria-required="true" required>
           <input type="email" name="email" placeholder="Email" class="form-control" aria-required="true" required>
         </div>
         <br>
